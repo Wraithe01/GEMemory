a4dcfde7e815df33a8db00195d44cb131856e23e
DANIEL HÄLL <dahl20@student.bth.se>
2023-11-22 19:56:21 +0100

    chore(main): Better example / test case

--- GEMemory/main.cpp
+++ GEMemory/main.cpp
@@ -5,7 +5,20 @@ auto main(void) -> int
 {
     StackAlloc salloc;
     MemRegion  mem = salloc.Alloc(DEFAULT_MEM_SIZE);
+
+    // Should not give any output
     if (!mem.IsValid())
         std::cout << "Invalid memory\n";
+    salloc.Free(&mem);
+
+
+    // Should print once
+    size_t size = 64;
+    for (uint32_t i = 0; i < DEFAULT_MEM_SIZE / size + 1; ++i)
+    {
+        if (!salloc.Alloc(size).IsValid())
+            std::cout << "Unable to allocate memory.\n";
+    }
+
     return 0;
 }
