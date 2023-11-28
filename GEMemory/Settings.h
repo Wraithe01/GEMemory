1d94474d874a929910f4c135c1db25e40613f5c6
DANIEL HÄLL <dahl20@student.bth.se>
2023-11-28 14:52:06 +0100

    fix(Pool): Last node is now considered ocupied for the allocator.

--- GEMemory/Settings.h
+++ GEMemory/Settings.h
@@ -1,6 +1,8 @@
 #pragma once
 
 // In bytes
-constexpr auto DEFAULT_MEM_SIZE  = 40'960;
+constexpr auto DEFAULT_MEM_SIZE = 40'960;
 // Data node in bytes
 constexpr auto DEFAULT_NODE_SIZE = 64;
+// This cannot be equal to 0
+constexpr auto POOL_OCCUPIED = 1;
