df03a17e746c2ab55f6058db542037caeb69761a
nick0900 <nicolas.angnell@hotmail.se>
2023-12-07 14:47:38 +0100

    feat(RM/main): setup resource manager project and hello world in main

---
+++ GEResourceManager/Includes.h
@@ -0,0 +1,11 @@
+#pragma once
+// Resource manager includes
+// 
+// This is a file which includes all of the essentials.
+// If you find youself needing a include in more than one file, include it here instead of including
+// it twice. For simplicity.
+//
+#pragma once
+#include "Settings.h"
+
+#include <iostream>
