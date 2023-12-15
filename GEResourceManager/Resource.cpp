48f33c470a9cd72e27413f716835666161de5f1e
DANIEL HÄLL <dahl20@student.bth.se>
2023-12-15 16:32:30 +0100

    feat(Resource): Added resource loading for Mesh (fbx) and Texture (Jpg, Png)

---
+++ GEResourceManager/Resource.cpp
@@ -0,0 +1,41 @@
+#include "Settings.h"
+#include "Resource.h"
+#include "ufbx.h"
+#include "stb_image.h"
+
+uint32_t  Resource::Decrement() { return --m_refc; }
+uint32_t& Resource::operator--() { return --m_refc; }
+uint32_t  Resource::operator--(int) { return m_refc--; }
+
+uint32_t  Resource::Increment() { return ++m_refc; }
+uint32_t& Resource::operator++() { return ++m_refc; }
+uint32_t  Resource::operator++(int) { return m_refc++; }
+
+
+bool Mesh::LoadResource(const std::string& filepath)
+{
+    ufbx_error err;
+    m_data = ufbx_load_file(filepath.c_str(), NULL, &err);
+    if (!m_data)
+    {
+        std::cerr << "Failed to load mesh (fbx) in path \"" << filepath
+                  << "\", with error: " << err.description.data << std::endl;
+        return false;
+    }
+    return true;
+}
+void Mesh::UnloadResource() { ufbx_free_scene(m_data); }
+
+
+bool Texture::LoadResource(const std::string& filepath)
+{
+    m_img = stbi_load(
+        filepath.c_str(), &m_dim.width, &m_dim.height, &m_dim.channels, img_max_channels);
+    if (!m_img)
+    {
+        std::cerr << "Failed to load texture (png) in path \"" << filepath << "\"." << std::endl;
+        return false;
+    }
+    return true;
+}
+void Texture::UnloadResource() { delete m_img; }
---
+++ GEResourceManager/Resource.h
@@ -0,0 +1,64 @@
+#pragma once
+#include "Includes.h"
+
+
+class Resource
+{
+public: // Methods
+    Resource() = default;
+    ~Resource() = default;
+
+    virtual bool LoadResource(const std::string& filepath) = 0;
+    virtual void UnloadResource()                   = 0;
+
+    // Reference counter functionality
+
+    virtual uint32_t Decrement();
+    virtual uint32_t& operator--(); // Pre
+    virtual uint32_t  operator--(int); // Post
+
+    virtual uint32_t Increment();
+    virtual uint32_t& operator++(); // Pre
+    virtual uint32_t  operator++(int); // Post
+
+private:
+
+protected: // Variables
+    uint32_t m_GUID;
+    uint32_t m_refc;
+};
+
+
+class Mesh : public Resource
+{
+public: // Methods
+    Mesh() = default;
+    ~Mesh() = default;
+
+    virtual bool LoadResource(const std::string& filepath) override;
+    virtual void UnloadResource() override;
+
+private:
+private: // Variables
+    ufbx_scene* m_data;
+};
+
+class Texture : public Resource
+{
+public:  // Methods
+    Texture() = default;
+    ~Texture() = default;
+
+    virtual bool LoadResource(const std::string& filepath) override;
+    virtual void UnloadResource() override;
+
+private:
+private:  // Variables
+    uint8_t* m_img;
+    struct
+    {
+        int32_t width;
+        int32_t height;
+        int32_t channels;
+    } m_dim;
+};
