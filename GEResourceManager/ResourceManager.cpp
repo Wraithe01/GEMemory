8d204bbb6424b66004c9507a8dc2467aac728269
DANIEL HÄLL <dahl20@student.bth.se>
2023-12-15 20:54:26 +0100

    style(Allman): format

--- GEResourceManager/ResourceManager.cpp
+++ GEResourceManager/ResourceManager.cpp
@@ -42,53 +42,6 @@ void ResourceManager::LoadScene(const Scene& scene)
         }
     }
 }
-
-
-void ResourceManager::LoadHeader()
-{
-    // Read the JSON file
-    std::ifstream file("../Packages/header.json");
-    if (!file.is_open())
-    {
-        std::cerr << "Failed to open JSON file\n";
-        return;
-    }
-    // Parse JSON
-    nlohmann::json jsonData;
-    try
-    {
-        file >> jsonData;
-    }
-    catch (const std::exception& e)
-    {
-        std::cerr << "Error parsing JSON: " << e.what() << "\n";
-        return;
-    }
-
-    for (const auto& entry : jsonData.items())
-    {
-        const std::string& guid     = entry.key();
-        const std::string& filename = entry.value()["filename"];
-        const std::string& package = entry.value()["package"];
-        const uLong offset = entry.value()["filepos"];
-        const uLong fileNumber = entry.value()["filenumber"];
-        const unz_file_pos_s filePos{ offset, fileNumber };
-
-        HeaderEntry entryData{ filename, package, filePos };
-        headerMap[guid] = entryData;
-    }
-}
-std::string ResourceManager::GetPackage(const std::string& guid)
-{
-    const auto& it = m_headerMap.find(guid);
-
-    if (it != m_headerMap.end())
-    {
-        return it->second.package;
-    }
-    return "";
-}
-
 void ResourceManager::ParseResource(const std::string& guid, const std::string& package)
 {
     // Get the type of file
@@ -115,4 +68,49 @@ void ResourceManager::ParseResource(const std::string& guid, const std::string& 
     //        break;
     //}
     // m_loadedData[guid]->InitRefcount();
+}
+
+void ResourceManager::LoadHeader()
+{
+    // Read the JSON file
+    std::ifstream file("../Packages/header.json");
+    if (!file.is_open())
+    {
+        std::cerr << "Failed to open JSON file\n";
+        return;
+    }
+    // Parse JSON
+    nlohmann::json jsonData;
+    try
+    {
+        file >> jsonData;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Error parsing JSON: " << e.what() << "\n";
+        return;
+    }
+
+    for (const auto& entry : jsonData.items())
+    {
+        const std::string&   guid       = entry.key();
+        const std::string&   filename   = entry.value()["filename"];
+        const std::string&   package    = entry.value()["package"];
+        const uLong          offset     = entry.value()["filepos"];
+        const uLong          fileNumber = entry.value()["filenumber"];
+        const unz_file_pos_s filePos{ offset, fileNumber };
+
+        HeaderEntry entryData{ filename, package, filePos };
+        m_headerMap[guid] = entryData;
+    }
+}
+std::string ResourceManager::GetPackage(const std::string& guid)
+{
+    const auto& it = m_headerMap.find(guid);
+
+    if (it != m_headerMap.end())
+    {
+        return it->second.package;
+    }
+    return "";
 }
