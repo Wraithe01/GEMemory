ea7fca6fcc3fa6b81dc89f877f40ab7b1d8c9ddf
File14 <filip.kjellsson@gmail.com>
2023-12-15 15:42:09 +0100

    Load header and put content into map

--- GEResourceManager/ResourceManager.h
+++ GEResourceManager/ResourceManager.h
@@ -1,3 +1,25 @@
 #pragma once
 
+#include <unordered_map>
+#include <string>
+#include <fstream>
+#include <json.hpp>
 
+#include "FileSystem.h"
+#include "guiddef.h"
+
+struct HeaderEntry {
+    std::string filename;
+    std::string package;
+};
+
+class ResourceManager
+{
+private:
+	std::unordered_map<std::string, HeaderEntry> headerMap;
+
+public:
+    void LoadHeader();
+
+    std::string GetPackage(const std::string& guid);
+};
