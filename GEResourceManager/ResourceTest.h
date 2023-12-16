2c0a35743ef72fc9f24e27c3a8910f76562fea2c
File14 <filip.kjellsson@gmail.com>
2023-12-16 18:15:13 +0100

    Add resourcetest.h

--- GEResourceManager/ResourceTest.h
+++ GEResourceManager/ResourceTest.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#include <chrono>
+#include "Scene.h"
+#include "ResourceManager.h"
+
+// To construct guid pattern
+#include <iomanip>
+#include <sstream>
+
+class ResourceTest {
+
+public:
+    ResourceTest();
+    ~ResourceTest();
+
+    void Validate();
+
+    void PerformanceBenchmark();
+};
