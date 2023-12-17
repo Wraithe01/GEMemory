1c0a9d1d3ed92639cfbd88643de4bade92f6cd66
rasmuspeterssonskola <rp.peterssonskola@gmail.com>
2023-12-17 22:36:22 +0100

    Removed .gz and added some guid changes needed for tar-reading

--- PackagingTool/PackagingTool.h
+++ PackagingTool/PackagingTool.h
@@ -16,10 +16,10 @@ class PackagingTool {
 public:
     virtual ~PackagingTool() {}
 
-    virtual void createPackage(const std::vector<std::string>& assets, const std::string& outputPath) = 0;
+    virtual void createPackage(std::vector<std::string>& assets, const std::string& outputPath) = 0;
 
     static void generateGUID(GUID& guid);
 
-    static void createHeaderFile(const std::vector<std::string>& assets, const std::string& headerFilePath,
+    static void createHeaderFile(std::vector<std::string>& assets, const std::string& headerFilePath,
         const std::string& packageName);
 };
