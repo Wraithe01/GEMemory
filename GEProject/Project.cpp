d9896c27a60e684ffea3747dd648965ae0b6bc81
DANIEL HÄLL <wraithe01@hotmail.com>
2024-01-07 22:26:02 +0100

    fix(ResourceManager): Exposed stack allocator to imgui and moved flush to end of frame

--- GEProject/Project.cpp
+++ GEProject/Project.cpp
@@ -14,8 +14,6 @@ constexpr auto HEIGHT = 1920;
 constexpr auto WIDTH  = 1080;
 
 std::set<ImAllocator*> g_imAlloc;
-// StackAlloc             g_frameStack;
-// PoolAlloc              g_poolAlloc;
 
 static ImAllocator* ImRegisterAllocator(Allocator* allocator);
 static void         ImUnregisterAllocator(ImAllocator* allocator);
@@ -24,29 +22,33 @@ static void         ImGuiMemoryTrace(void);
 
 void Run()
 {
+    ResourceManager& resourceManager = ResourceManager::GetInstance();
+
     //- Initialization
     RendererInit(HEIGHT, WIDTH);  // This must be working before imgui can work
     ImguiInit();
 
+    ImRegisterAllocator(resourceManager.GetStack());
+
     // Load basic lighting shader
-    Shader shader = LoadShader(TextFormat("../shaders/lighting.vs"), TextFormat("../shaders/lighting.fs"));
+    Shader shader
+        = LoadShader(TextFormat("../shaders/lighting.vs"), TextFormat("../shaders/lighting.fs"));
 
     // Get some required shader locations
     shader.locs[SHADER_LOC_VECTOR_VIEW] = GetShaderLocation(shader, "viewPos");
 
-    int ambientLoc = GetShaderLocation(shader, "ambient");
-    float ambient[4] = {0.1f, 0.1f, 0.1f, 1.0f};
+    int   ambientLoc = GetShaderLocation(shader, "ambient");
+    float ambient[4] = { 0.1f, 0.1f, 0.1f, 1.0f };
     SetShaderValue(shader, ambientLoc, &ambient, SHADER_UNIFORM_VEC4);
-   
-    Light lights[MAX_LIGHTS] = { 0 };
-    Vector3 pos = { 0, 1, 0 };
-    lights[0] = CreateLight(LIGHT_DIRECTIONAL, pos, { 1, 0, 0 }, RED, shader);
-    lights[1] = CreateLight(LIGHT_DIRECTIONAL, pos, { 0, 0, 1 }, GREEN, shader);
-    lights[2] = CreateLight(LIGHT_DIRECTIONAL, pos, { -1, 0, 0 }, BLUE, shader);
-    lights[3] = CreateLight(LIGHT_DIRECTIONAL, pos, { 0, 1, -1 }, YELLOW, shader);
+
+    Light   lights[MAX_LIGHTS] = { 0 };
+    Vector3 pos                = { 0, 1, 0 };
+    lights[0]                  = CreateLight(LIGHT_DIRECTIONAL, pos, { 1, 0, 0 }, RED, shader);
+    lights[1]                  = CreateLight(LIGHT_DIRECTIONAL, pos, { 0, 0, 1 }, GREEN, shader);
+    lights[2]                  = CreateLight(LIGHT_DIRECTIONAL, pos, { -1, 0, 0 }, BLUE, shader);
+    lights[3]                  = CreateLight(LIGHT_DIRECTIONAL, pos, { 0, 1, -1 }, YELLOW, shader);
 
     // Default scenes
-    ResourceManager& resourceManager = ResourceManager::GetInstance();
     Scene scenes[6] = {};
     scenes[0].AppendChunk("chunk1");
     scenes[1].AppendChunk("chunk2");
@@ -55,33 +57,34 @@ void Run()
     scenes[4].AppendChunk("chunk5");
     scenes[5].AppendChunk("chunk6");
     std::string pngTexture = "702e3416-3cc1-4ae5-8f7e-1a439bc2951f";
-    scenes[0].AppendGUID(pngTexture); //PNG for texture in scene1 (fbx scene, DJ board people)
+    scenes[0].AppendGUID(pngTexture);  // PNG for texture in scene1 (fbx scene, DJ board people)
 
     // Load scenes
-    for (auto& scene : scenes) {
+    for (auto& scene : scenes)
+    {
         const auto& asyncRequest = resourceManager.LoadScene(scene);
         resourceManager.AsynchRequestWait(asyncRequest);
     }
     ITexture* myTexture = resourceManager.GetTexture(pngTexture).get();
-    int size = myTexture->GetWidth() * myTexture->GetHeight() * myTexture->GetChannels();
+    int       size      = myTexture->GetWidth() * myTexture->GetHeight() * myTexture->GetChannels();
 
     Image image{ 0 };
-    image.width = myTexture->GetWidth();
-    image.height = myTexture->GetHeight();
-    image.data = (void*)myTexture->GetImage();
-    image.format = PIXELFORMAT_UNCOMPRESSED_R8G8B8;
+    image.width   = myTexture->GetWidth();
+    image.height  = myTexture->GetHeight();
+    image.data    = (void*) myTexture->GetImage();
+    image.format  = PIXELFORMAT_UNCOMPRESSED_R8G8B8;
     image.mipmaps = 1;
 
     Texture2D texture = LoadTextureFromImage(image);
 
     // Default material
-    Material matDefault = LoadMaterialDefault();
+    Material matDefault                           = LoadMaterialDefault();
     matDefault.maps[MATERIAL_MAP_DIFFUSE].texture = texture;
-    matDefault.shader = shader;
+    matDefault.shader                             = shader;
 
     // Default transform
-    Matrix rotation = MatrixRotateX(DEG2RAD * 270.0f);
-    Matrix scale = MatrixScale(1.5f, 1.5f, 1.5f);
+    Matrix rotation  = MatrixRotateX(DEG2RAD * 270.0f);
+    Matrix scale     = MatrixScale(1.5f, 1.5f, 1.5f);
     Matrix transform = MatrixMultiply(scale, rotation);
 
     int demoTimer = 0;
@@ -112,7 +115,8 @@ void Run()
         SetShaderValue(shader, shader.locs[SHADER_LOC_VECTOR_VIEW], cameraPos, SHADER_UNIFORM_VEC3);
 
         // Update light values
-        for (int i = 0; i < MAX_LIGHTS; i++) {
+        for (int i = 0; i < MAX_LIGHTS; i++)
+        {
             UpdateLightValues(shader, lights[i]);
         }
 
@@ -127,8 +131,8 @@ void Run()
         for (const auto& pair : *resourceManager.GetLoadedMeshes())
         {
             const std::string& resourceName = pair.first;
-            IMesh* resource = pair.second.get();
-            size_t count = resource->GetMeshCount();
+            IMesh*             resource     = pair.second.get();
+            size_t             count        = resource->GetMeshCount();
 
             for (size_t i = 0; i < count; i++)
             {
@@ -144,7 +148,8 @@ void Run()
         ImGuiMemoryTrace();
         ImguiEnd();
 
-        // g_frameStack.Flush();
+        ((StackAlloc*) (resourceManager.GetStack()))->Flush();
+
         EndMode3D();
 
         DrawText("Press ESC to exit", 50, 50, 50, BLUE);
@@ -153,10 +158,10 @@ void Run()
     }
 
     //- De-Initialization
-    UnloadShader(shader);   // Unload shader
-    UnloadTexture(texture); // Unload default texture
+    UnloadShader(shader);    // Unload shader
+    UnloadTexture(texture);  // Unload default texture
 
-    //RaylibImGui::Deinit();
+    // RaylibImGui::Deinit();
     ImguiDeInit();
     CloseWindow();  // Close window and OpenGL context
 }
@@ -217,7 +222,7 @@ static void ImGuiResourceTrace(void)
             ImPlot::SetupAxes(
                 nullptr, nullptr, ImPlotAxisFlags_NoTickLabels, ImPlotAxisFlags_NoTickLabels);
             ImPlot::SetupAxisLimits(ImAxis_X1, time - span, time, ImGuiCond_Always);
-            ImPlot::SetupAxisLimits(ImAxis_Y1, -1, maxMeshes + 1, ImGuiCond_::ImGuiCond_Always);
+            ImPlot::SetupAxisLimits(ImAxis_Y1, -1, maxMeshes * 1.1f, ImGuiCond_::ImGuiCond_Always);
 
             ImPlot::PlotLine("Loaded Meshes",
                              &meshBuffer.Data[0].x,
@@ -256,7 +261,8 @@ static void ImGuiResourceTrace(void)
             ImPlot::SetupAxes(
                 nullptr, nullptr, ImPlotAxisFlags_NoTickLabels, ImPlotAxisFlags_NoTickLabels);
             ImPlot::SetupAxisLimits(ImAxis_X1, time - span, time, ImGuiCond_Always);
-            ImPlot::SetupAxisLimits(ImAxis_Y1, -1, maxTextures + 1, ImGuiCond_::ImGuiCond_Always);
+            ImPlot::SetupAxisLimits(
+                ImAxis_Y1, -1, maxTextures * 1.1f, ImGuiCond_::ImGuiCond_Always);
 
             ImPlot::PlotLine("Loaded Textures",
                              &texBuffer.Data[0].x,
